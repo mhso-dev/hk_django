12af07a0026ee83b4e6694e52ccc963632d3a655
Minho So <mhso.developer@gmail.com>
2021-11-06 11:11:02 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=30)  # 문자열의 최대길이 30으로 지정
+    content = models.TextField()  # 문자열의 길이 제한이 없음
+
+    created_at = models.DateTimeField(auto_now_add=True)  # 월, 일, 시, 분, 초 기록
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
